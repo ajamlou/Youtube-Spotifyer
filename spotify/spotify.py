d13632f44804ea65aa310e6a69f75eada5a84b67
Max <max.netterberg@gmail.com>
2020-03-03 20:40:38 +0100

    whatever

---
+++ spotify/spotify.py
@@ -0,0 +1,6 @@
+
+
+class Spotify:
+    client_id = "6cb010cfc3774f8ca5247a501664a548"
+    client_secret = "d99c3963895a4f029e1829443ff7b9e9"
+    redirect_uri = "https://google.com/"
--- spotify/spotifyendpoint.py
+++ spotify/spotifyendpoint.py
@@ -1,8 +1,24 @@
-def get_spotify_url(self):
-    pass
+import os
+import sys
+import json
+import spotipy
+import webbrowser
+import spotipy.util as util
+import spotify
+from json.decoder import JSONDecodeError
 
-def create_playlist(self):
-    pass
 
-def add_song_to_playlist(self):
-    pass
+username = "kodstuga"
+scope = 'user-read-private user-read-playback-state user-modify-playback-state'
+
+Spotify = spotify.Spotify()
+
+try:
+    token = util.prompt_for_user_token(
+        username, scope, Spotify.client_id, Spotify.client_secret, Spotify.redirect_uri)  # add scope
+except (AttributeError, JSONDecodeError):
+    os.remove(f".cache-{username}")
+    token = util.prompt_for_user_token(
+        username, scope, Spotify.client_id, Spotify.client_secret, Spotify.redirect_uri)  # add scope
+
+spotifyObject = spotipy.Spotify(auth=token)

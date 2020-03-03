3d470d7629811915957601a10b9316537921d833
Max <max.netterberg@gmail.com>
2020-03-03 21:55:30 +0100

    func beach

--- spotify/spotifyendpoint.py
+++ spotify/spotifyendpoint.py
@@ -9,7 +9,7 @@ from json.decoder import JSONDecodeError
 
 
 username = "kodstuga"
-scope = 'user-read-private user-read-playback-state user-modify-playback-state'
+scope = 'user-read-private user-read-playback-state user-modify-playback-state playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private'
 
 Spotify = spotify.Spotify()
 
@@ -33,8 +33,33 @@ while True:
     print()
     print("0 - Search for an artist")
     print("1 - exit")
+    print("2 - add playlist")
+    print("3 - add song to playlist")
     print()
     choice = input("Your choice: ")
 
-    if choice == 1:
+    if choice == "0":
+        query = input("Alright beach! What is the name of the artist?: ")
+        searchResults = spotifyObject.search(query, 1, 0, "artist")
+        artist = searchResults['artists']['items'][0]
+        print(artist['name'])
+        print(str(artist['followers']['total']) + " followers")
+        print(artist['genres'][0])
+        print()
+
+    if choice == "1":
         break
+
+    if choice == "2":
+        name = input("Okey mateyo what is the name of the playlist? ")
+        spotifyObject.user_playlist_create(user["id"], name)
+
+    if choice == "3":
+        query_song = input("Okey dude. What is the name of the song? ")
+        query_artist = input("Okey dude. What is the artist of the song? ")
+        searchResults = spotifyObject.search(
+            q='artist:'+query_artist+' track:'+query_song)
+        song_uri = searchResults['tracks']['items'][0]['uri']
+        playlist = spotifyObject.user_playlists(user['id'])['items'][0]['id']
+        spotifyObject.user_playlist_add_tracks(
+            user['id'], playlist_id=playlist, tracks=[song_uri])
